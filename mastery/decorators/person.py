1a2c0080517adb3baee87c44441905355c8667cd
YeongSeok Kim <youngstone89@icloud.com>
2022-05-20 14:17:49 +0900

    added comments

--- mastery/decorators/person.py
+++ mastery/decorators/person.py
@@ -8,6 +8,7 @@ class Person:
         self.age = age
       
     # a class method to create a Person object by birth year.
+    # class method takes cls as the first parameter while a static method needs no specific parameters
     @classmethod
     def fromBirthYear(cls, name, year):
         return cls(name, date.today().year - year)
@@ -18,10 +19,14 @@ class Person:
         return age > 18
   
 person1 = Person('mayank', 21)
+
 person2 = Person.fromBirthYear('mayank', 1996)
-  
+
+person3 = person1.fromBirthYear('youngstone89',1989)
+
 print (person1.age)
 print (person2.age)
-  
+print (person3.age)
+
 # print the result
 print (Person.isAdult(22))
