68eac0a4e5609f862db981dbde3dfceab16c88fe
YeongSeok Kim <youngstone89@icloud.com>
2022-05-20 14:35:11 +0900

    iterators

---
+++ mastery/classes/iterators.py
@@ -0,0 +1,40 @@
+for element in [1, 2, 3]:
+    print(element)
+for element in (1, 2, 3):
+    print(element)
+for key in {'one':1, 'two':2}:
+    print(key)
+for char in "123":
+    print(char)
+# for line in open("myfile.txt"):
+#     print(line, end='')
+
+
+s = 'abc'
+it = iter(s)
+print(next(it))
+print(next(it))
+print(next(it))
+# print(next(it)) # StopIteration exception 
+
+
+class Reverse:
+    """Iterator for looping over a sequence backwards."""
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1
+        return self.data[self.index]
+
+rev = Reverse('spam')
+revIterator = iter(rev)
+
+for char in revIterator:
+    print(char)
