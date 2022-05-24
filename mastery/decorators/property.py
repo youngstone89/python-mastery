814a6b1b69a870cc133a97cf828f68cf0de70c2a
YeongSeok Kim <youngstone89@icloud.com>
2022-05-24 13:32:17 +0900

    added @property decorators

---
+++ mastery/decorators/property.py
@@ -0,0 +1,26 @@
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
+
+    @property
+    def radius(self):
+        """The radius property."""
+        print("Get radius")
+        return self._radius
+
+    @radius.setter
+    def radius(self, value):
+        print("Set radius")
+        self._radius = value
+
+    @radius.deleter
+    def radius(self):
+        print("Delete radius")
+        del self._radius
+
+circle = Circle(42.0)
+print(circle.radius)
+circle.radius = 100.0
+print(circle.radius)
+del circle.radius
+print(circle.radius)
